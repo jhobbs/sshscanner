2f6794106f8b854251f7454a21f3649aac6558ca
Jason Hobbs <jason.hobbs@gmail.com>
2017-12-22 22:12:28 +0000

    Now runs concurrently.

--- sshscanner.go
+++ sshscanner.go
@@ -1,7 +1,9 @@
 package main
 
 import "fmt"
+import "io"
 import "os"
+import "math"
 import "net"
 
 // Increments an IP address.  Only works for Ipv4.
@@ -17,14 +19,13 @@ func increment_ip(ip net.IP) net.IP {
 
 		v4[i] = 0
 	}
-	fmt.Println(v4[3])
 	return v4
 }
 
-func scan_address(ip net.IP) {
+func scan_address(ip net.IP, ch chan<- string) {
 	conn, err := net.Dial("tcp", ip.String()+":22")
 	if err != nil {
-		fmt.Printf("Connection error: %s\n", err)
+		ch <- fmt.Sprintf("Connection error: %s\n", err)
 		return
 	}
 
@@ -32,10 +33,17 @@ func scan_address(ip net.IP) {
 	for {
 		n, err := conn.Read(output)
 		if err != nil {
-			fmt.Printf("Read error: %s\n", err)
-			break
+			if err != io.EOF {
+				ch <- fmt.Sprintf("read error: %s\n", err)
+				ch <- fmt.Sprintf("%s: read error %s\n", ip, err)
+				break
+			}
+			if n == 0 {
+				ch <- fmt.Sprintf("%s: EOF with no data\n", ip)
+				break
+			}
 		}
-		fmt.Printf("%s %s", ip.String(), output[:n])
+		ch <- fmt.Sprintf("%s %s", ip.String(), output[:n])
 		if n > 0 {
 			break
 		}
@@ -45,8 +53,17 @@ func scan_address(ip net.IP) {
 
 func scan_subnet(subnet_cidr string) {
 	address, network, _ := net.ParseCIDR(subnet_cidr)
+	ch := make(chan string)
 	for ; network.Contains(address); address = increment_ip(address) {
-		scan_address(address)
+		dup := make(net.IP, len(address))
+		copy(dup, address)
+		go scan_address(dup, ch)
+	}
+
+	bits, _ := network.Mask.Size()
+	size := int(math.Pow(2, float64(32-bits)))
+	for i := 0; i < size-1; i++ {
+		fmt.Print(<-ch)
 	}
 }
 
